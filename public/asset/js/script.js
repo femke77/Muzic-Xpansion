7ab36613cf3364a205cf0e0223a447ac843bf357
Kfriedman <Koryf91@gmail.com>
2019-11-23 14:33:19 -0800

    js for viedo

--- public/asset/js/script.js
+++ public/asset/js/script.js
@@ -8,20 +8,48 @@ var firebaseConfig = {
     messagingSenderId: "280884255831",
     appId: "1:280884255831:web:9b5914a751d3d6fb0f9712",
     measurementId: "G-RFFRMPBZLJ"
-  };
+};
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
-  //Firebase database ref
-  var database = firebase.database();
+//Firebase database ref
+
+$(document).ready(function () {
+
+    console.log("test")
+
+});
 
 
-  $(document).ready(function(){
-      console.log("test")
+var database = firebase.database();
+var input = $(".form-control");
+var submit = $("#ytSubmit");
+var apiKey = "AIzaSyAfNZnAU5IoLkNDkr3zbWGhWLJJcDwd7rI";
+
+$("#ytSubmit").on("click", function (event) {
+    // event.preventdefault();
+    var inputVal = input.val().trim();
+    var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q=" + inputVal + "&key=AIzaSyAfNZnAU5IoLkNDkr3zbWGhWLJJcDwd7rI";
 
 
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
 
+        console.log(queryURL);
+        console.log(response);
 
-  });
+        var videoId = response.items[0].id.videoId;
+        console.log(videoId)
+
+        var video = $("<iframe allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen>");
+
+        video.attr("src", "https://www.youtube.com/embed/" + videoId);
+        $("#video").append(video)
+
+    });
+
+})
