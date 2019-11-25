3120fb39bc2a06e8cb74a7620a469e5e339fe51a
Meg Meyers <megan.g.meyers@gmail.com>
2019-11-24 20:34:56 -0800

    error handling and refactoring

--- public/asset/js/script.js
+++ public/asset/js/script.js
@@ -18,22 +18,18 @@ firebase.analytics();
 var database = firebase.database();
 
 
-
-
-
 $(document).ready(function () {
 
-    // var queryURL2 = "https://api.seatgeek.com/2/performers?q=red+hot+chili+peppers&client_id=MTk1ODIzNzZ8MTU3NDM5NzAwNy40NQ"
-    // var queryURL3 = "https://api.seatgeek.com/2/events?performers.slug=red-hot-chili-peppers&client_id=MTk1ODIzNzZ8MTU3NDM5NzAwNy40NQ"
+    var key = "MTk1ODIzNzZ8MTU3NDM5NzAwNy40NQ";
     var input;
-
 
 
     $("#submit").on("click", function (e) {
         e.preventDefault();
         input = $("#test").val().trim();
-        var queryURLPerformers = "https://api.seatgeek.com/2/performers?q=" + input +
-            "&client_id=MTk1ODIzNzZ8MTU3NDM5NzAwNy40NQ"
+        input = input.split(' ').join('-');
+        var queryURLPerformers = "https://api.seatgeek.com/2/performers?slug=" + input +
+            "&client_id=" + key;
         console.log(input);
         console.log(queryURLPerformers);
         $.ajax({
@@ -41,32 +37,51 @@ $(document).ready(function () {
             method: "GET"
         })
             .then(function (response) {
-                var hasEvent = response.performers[0].has_upcoming_events;
-                console.log("performer has event?" + hasEvent);
-                if (hasEvent === true){
-                    input = input.split(' ').join('-');
-                    var queryURLEvents = "https://api.seatgeek.com/2/events?performers.slug=" + input + 
-                        "&client_id=MTk1ODIzNzZ8MTU3NDM5NzAwNy40NQ"
-                    console.log(input)
-                    console.log(queryURLEvents);
-                    $.ajax({
-                        url: queryURLEvents,
-                        method: "GET"
-                    })
-                        .then(function (response) {
-                            console.log(response);
-                            var results = response.events;
-                            for (let i = 0; i < results.length; i++) {
-                                console.log("result " + results[i].title);
-                                
-                            }
-                            
-
-                        });
-
-                }
+                console.log(response)
+                if (response.performers.length === 0){
+                    console.log("Performer not found");
+                } else {
+                    var hasEvent = response.performers[0].has_upcoming_events;
+                    console.log("performer has event? " + hasEvent);
+                    if (hasEvent === true) {
+                        getEvents();
+                    } else {
+                        console.log("No upcoming events for this performer.");
+                    }
+                } 
             });
-
     });
 
-});
+
+    function getEvents() {
+        
+        var queryURLEvents = "https://api.seatgeek.com/2/events?performers.slug=" + input +
+            "&client_id=" + key;
+        console.log(input)
+        console.log(queryURLEvents);
+        $.ajax({
+            url: queryURLEvents,
+            method: "GET"
+        })
+            .then(function (response) {
+                console.log(response);
+                //pull images too and if date_tbd == false go forward with date time otherwise say TBD on card
+                var results = response.events;
+                for (let i = 0; i < results.length; i++) {
+                    var dateTime = results[i].datetime_local;
+                    dateTime = dateTime.split('T').join(' ');
+                    var format = "YYYY/MM/DD hh:mm:ss";
+                    var convertedDateTime = moment(dateTime, format)
+                    console.log("title: " + results[i].title);
+                    console.log("date & time: " + convertedDateTime.format("MM/DD/YY hh:mm A"));
+                    console.log("where: " + results[i].venue.display_location);
+                    console.log("tickets: " + results[i].url);
+                }
+            })
+    }
+
+
+
+
+
+}); //end document.ready
