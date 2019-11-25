54691b4e19c9c6a49fea6f89fea52f8f57b6a557
Meg Meyers <megan.g.meyers@gmail.com>
2019-11-24 21:02:02 -0800

    date time tbd handling and documentation of functions

--- public/asset/js/script.js
+++ public/asset/js/script.js
@@ -17,71 +17,76 @@ firebase.analytics();
 //Firebase database ref
 var database = firebase.database();
 
+var key = "MTk1ODIzNzZ8MTU3NDM5NzAwNy40NQ";
+var input;
 
-$(document).ready(function () {
-
-    var key = "MTk1ODIzNzZ8MTU3NDM5NzAwNy40NQ";
-    var input;
-
-
-    $("#submit").on("click", function (e) {
-        e.preventDefault();
-        input = $("#test").val().trim();
-        input = input.split(' ').join('-');
-        var queryURLPerformers = "https://api.seatgeek.com/2/performers?slug=" + input +
-            "&client_id=" + key;
-        console.log(input);
-        console.log(queryURLPerformers);
-        $.ajax({
-            url: queryURLPerformers,
-            method: "GET"
-        })
-            .then(function (response) {
-                console.log(response)
-                if (response.performers.length === 0){
-                    console.log("Performer not found");
+/**
+ * Gets input from user search box, uses ajax get to hit Seat Geek api /performer endpoint
+ * and find out if performer exists and has event coming up
+ */
+$("#submit").on("click", function (e) {
+    e.preventDefault();
+    input = $("#test").val().trim();
+    input = input.split(' ').join('-');
+    var queryURLPerformers = "https://api.seatgeek.com/2/performers?slug=" + input +
+        "&client_id=" + key;
+    console.log(input);
+    console.log(queryURLPerformers);
+    $.ajax({
+        url: queryURLPerformers,
+        method: "GET"
+    })
+        .then(function (response) {
+            console.log(response)
+            if (response.performers.length === 0) {
+                console.log("Performer not found");
+            } else {
+                var hasEvent = response.performers[0].has_upcoming_events;
+                console.log("performer has event? " + hasEvent);
+                if (hasEvent === true) {
+                    getEvents();
                 } else {
-                    var hasEvent = response.performers[0].has_upcoming_events;
-                    console.log("performer has event? " + hasEvent);
-                    if (hasEvent === true) {
-                        getEvents();
-                    } else {
-                        console.log("No upcoming events for this performer.");
-                    }
-                } 
-            });
-    });
+                    console.log("No upcoming events for this performer.");
+                }
+            }
+        });
+});
 
-
-    function getEvents() {
-        
-        var queryURLEvents = "https://api.seatgeek.com/2/events?performers.slug=" + input +
-            "&client_id=" + key;
-        console.log(input)
-        console.log(queryURLEvents);
-        $.ajax({
-            url: queryURLEvents,
-            method: "GET"
-        })
-            .then(function (response) {
-                console.log(response);
-                //pull images too and if date_tbd == false go forward with date time otherwise say TBD on card
-                var results = response.events;
-                for (let i = 0; i < results.length; i++) {
+/**
+ * Gets events from the Seat Geek /events endpoint using performers argument from user input box
+ * MomentJS is used to convert the date time to a more readable format
+ */
+function getEvents() {
+    var queryURLEvents = "https://api.seatgeek.com/2/events?performers.slug=" + input +
+        "&client_id=" + key;
+    console.log(input)
+    console.log(queryURLEvents);
+    $.ajax({
+        url: queryURLEvents,
+        method: "GET"
+    })
+        .then(function (response) {
+            console.log(response);
+            //pull images for card
+            var results = response.events;
+            for (let i = 0; i < results.length; i++) {
+                console.log("title: " + results[i].title);
+                if (results[i].date_tbd == false) {
                     var dateTime = results[i].datetime_local;
                     dateTime = dateTime.split('T').join(' ');
                     var format = "YYYY/MM/DD hh:mm:ss";
                     var convertedDateTime = moment(dateTime, format)
-                    console.log("title: " + results[i].title);
                     console.log("date & time: " + convertedDateTime.format("MM/DD/YY hh:mm A"));
-                    console.log("where: " + results[i].venue.display_location);
-                    console.log("tickets: " + results[i].url);
+                } else {
+                    console.log("date & time: TBD");
                 }
-            })
-    }
+                console.log("where: " + results[i].venue.display_location);
+                console.log("tickets: " + results[i].url);
+            }
+        })
+}
 
 
 
 
 
-}); //end document.ready
