e6856c5aa190b0cca7f76034e182809fb372bfee
Krizel <krizzlybear@Minnemac.local>
2019-11-26 21:31:51 -0800

    fixed error

--- public/asset/js/script.js
+++ public/asset/js/script.js
@@ -92,68 +92,7 @@ $("#ytSubmit").on("click", function (event) {
     var errorCode = error.code;
     var errorMessage = error.message;
 
-    function formValidation() {
-      var myInput = document.getElementById("psw");
-      var letter = document.getElementById("letter");
-      var capital = document.getElementById("capital");
-      var number = document.getElementById("number");
-      var length = document.getElementById("length");
-
-// When the user clicks on the password field, show the message box
-  myInput.onfocus = function() {
-  document.getElementById("message").style.display = "block";
-}
-
-// When the user clicks outside of the password field, hide the message box
-  myInput.onblur = function() {
-  document.getElementById("message").style.display = "none";
-}
-
-// When the user starts to type something inside the password field
-myInput.onkeyup = function() {
-  // Validate lowercase letters
-  var lowerCaseLetters = /[a-z]/g;
-  if(myInput.value.match(lowerCaseLetters)) {
-    letter.classList.remove("invalid");
-    letter.classList.add("valid");
-  } else {
-    letter.classList.remove("valid");
-    letter.classList.add("invalid");
-}
-
-  // Validate capital letters
-  var upperCaseLetters = /[A-Z]/g;
-  if(myInput.value.match(upperCaseLetters)) {
-    capital.classList.remove("invalid");
-    capital.classList.add("valid");
-  } else {
-    capital.classList.remove("valid");
-    capital.classList.add("invalid");
-  }
-
-  // Validate numbers
-  var numbers = /[0-9]/g;
-  if(myInput.value.match(numbers)) {
-    number.classList.remove("invalid");
-    number.classList.add("valid");
-  } else {
-    number.classList.remove("valid");
-    number.classList.add("invalid");
-  }
-
-    // Validate characters
-    var characters = /[!@#$%^&*]/g;
-    if(myInput.value.match(characters)) {
-      characters.classList.remove("invalid");
-      characters.classList.add("valid");
-    } else {
-      characters.classList.remove("valid");
-      characters.classList.add("invalid");
-    }
-
-}
-    }
-  });
+   
 //       var email = document.getElementById("mail");
 
 // email.addEventListener("input", function (event) {
@@ -205,5 +144,5 @@ myInput.onkeyup = function() {
   // MODALS FOR LOGIN/SIGNUP ERRORS  
   // ==============
 
-
+  });
 })
