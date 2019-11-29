9f91bbe86498be2e36ec60263f84098d29a53c66
Krizel <krizzlybear@Minnemac.local>
2019-11-28 21:08:03 -0800

    fixing syntax errors within html form to help js work

--- public/asset/js/auth.js
+++ public/asset/js/auth.js
@@ -2,14 +2,14 @@
 // REGISTRATION FEATURE
 // ===============
 
-var registrationForm = document.querySelector("#registrationForm");
-registrationForm.addEventListener("submit", (event) => {
+var registrationForm = document.querySelector('#registrationForm');
+registrationForm.addEventListener('submit', (event) => {
 event.preventDefault();
 
-const email = registrationForm["registrationEmail"].value;
-const password = registrationForm["#createPassword"].value;
+var email = registrationForm['registrationEmail'].value;
+var password = registrationForm['createPassword'].value;
 
 console.log(email, password);
 
 
-})
+});
--- public/asset/js/script.js
+++ public/asset/js/script.js
@@ -25,7 +25,6 @@ var firebaseConfig = {
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-firebase.analytics();
 
 // Firebase database ref
 
