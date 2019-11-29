0d8c796c7d4b6c2b920a15533ea0c8941d2b27d0
Krizel <krizzlybear@Minnemac.local>
2019-11-28 21:48:48 -0800

    adding initial login logout js

--- public/asset/js/auth.js
+++ public/asset/js/auth.js
@@ -13,12 +13,42 @@ var password = registrationForm['createPassword'].value;
 // CREATE NEW USER
 firebase.auth().createUserWithEmailAndPassword(email, password).then(cred => {
     console.log(cred);
-    MktoForms2.whenReady(function (form){
-         form.onSuccess(function(values, followUpUrl){
-          form.getFormElem().hide();
-          document.getElementById('confirmForm').style.visibility = 'visible';
-          return false;
-        });
 })
+
+// SHOW CONFIRMATION DIV
+
+// PROMPT MODAL ERRORS
+
+// ===============
+// LOG-IN FEATURE
+// ===============
+
+var loginForm = document.querySelector('login');
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    // GRAB USER INFO
+    var email = loginForm['email'].value;
+    var password = loginForm['password'].value;
+
+    auth.signInWithEmailandPassword(email, password).then(cred => {
+        console.log(cred.user)
+        // Take user to the main page
+        
+    })
+
+})
+
+// ===============
+// LOG OUT FEATURE
+// ===============
+var logout = document.querySelector('#logout');
+logout.addEventListener('click', (event) => {
+    event.preventDefault;
+    auth.signOut().then(()=> {
+        console.log('User signed out');
+    })
+})
+
 
 });
