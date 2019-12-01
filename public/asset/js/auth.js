1f08c7cd026056939a1612c9bfdc421086166f4e
Krizel <krizzlybear@Minnemac.local>
2019-11-30 16:44:01 -0800

    fixing auth bugs and adding add'l pseudocode to complete user registration process

--- public/asset/js/auth.js
+++ public/asset/js/auth.js
@@ -15,6 +15,10 @@ firebase.auth().createUserWithEmailAndPassword(email, password).then(cred => {
     console.log(cred);
 })
 
+// CREATE MODAL ALERT IF USER EXISTS
+
+// CREATE MODAL ALERT IF PASSWORD DOESN'T MEET SPECIFICATIONS
+
 // SHOW CONFIRMATION DIV
 
 // PROMPT MODAL ERRORS
@@ -23,43 +27,32 @@ firebase.auth().createUserWithEmailAndPassword(email, password).then(cred => {
 // LOG-IN FEATURE
 // ===============
 
-var loginForm = document.querySelector('login');
-loginForm.addEventListener('submit', (event) => {
-    event.preventDefault();
+// var loginForm = document.querySelector('login');
+// loginForm.addEventListener('submit', (event) => {
+//     event.preventDefault();
 
-    // GRAB USER INFO
-    var email = loginForm['email'].value;
-    var password = loginForm['password'].value;
+//     // GRAB USER INFO
+//     var email = loginForm['email'].value;
+//     var password = loginForm['password'].value;
 
-    auth.signInWithEmailandPassword(email, password).then(cred => {
-        console.log(cred.user)
-        // Take user to the main page
+//     auth.signInWithEmailandPassword(email, password).then(cred => {
+//         console.log(cred.user)
+//         // Take user to the main page
         
-    })
+//     })
 
-})
+// })
 
 // ===============
 // LOG OUT FEATURE
 // ===============
-var logout = document.querySelector('#logout');
-logout.addEventListener('click', (event) => {
-    event.preventDefault;
-    auth.signOut().then(()=> {
-        console.log('User signed out');
-    })
-})
+// var logout = document.querySelector('#logout');
+// logout.addEventListener('click', (event) => {
+//     event.preventDefault;
+//     auth.signOut().then(()=> {
+//         console.log('User signed out');
+//     })
+// })
 
 
 });
-var registrationForm = document.querySelector("#registrationForm");
-registrationForm.addEventListener("submit", (event) => {
-event.preventDefault();
-
-const email = registrationForm["registrationEmail"].value;
-const password = registrationForm("#createPassword").value;
-
-console.log(email, password);
-
-
-})
