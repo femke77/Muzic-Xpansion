28024e08eb94601528d276828d83bd38bc603e59
Meg Meyers <megan.g.meyers@gmail.com>
2019-12-04 17:31:53 -0800

    change api key back to kory's

--- public/asset/js/auth.js
+++ public/asset/js/auth.js
@@ -27,13 +27,13 @@ $("#newUser").on("click", function (event) {
             if (errorCode === "auth/invalid-email" || errorCode === "auth/weak-password") {
                 $("#alert-reg-msg").html("Please input a valid email and a password of at least 6 characters.")
                 $("#alert-reg").show();
-             
+
             }
-            if (errorCode === "auth/email-already-in-use"){
+            if (errorCode === "auth/email-already-in-use") {
                 $("#alert-reg-msg").html("This email is already associated with an account. Please login.")
                 $("#alert-reg").show();
             }
-            
+
             $(".close").click(function () {
                 $("#alert-reg").hide();
 
@@ -48,49 +48,50 @@ $("#user").on("click", function (event) {
     var email = $("#loginEmail").val();
     var password = $("#userPassword").val();
     console.log(email + " " + password);
-    firebase.auth().signInWithEmailAndPassword(email, password).then(cred => {
-        console.log(cred.user)
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(cred => {
+            console.log(cred.user)
 
-        // CLOSE LOGIN MODAL AND RESET FORM
-        $("#login-modal").hide();
-        $(".modal-backdrop").remove();
-        $("form").trigger("reset");
-
-
-
-    }).catch(function (error) {
-        errorCode = error.code;
-        errorMsg = error.message;
-        if (errorCode === "auth/user-not-found") {
-
-            $("#alert-login-msg").html("Email not found. Have you registered yet?")
-            $("#alert-login").show();
-
-        }
-        if (errorCode === "auth/invalid-email") {
-
-            $("#alert-login-msg").html("Please input a valid email address.")
-            $("#alert-login").show();
-
-        }
-        if (errorCode === "auth/wrong-password") {
-
-            $("#alert-login-msg").html("Invalid password. Please check the password and try again.")
-            $("#alert-login").show();
-
-        }
-        if (errorCode === "auth/too-many-requests"){
-
-            $("#alert-login-msg").html("Too many requests. Please try again later.")
-            $("#alert-login").show();
-        }
+            // CLOSE LOGIN MODAL AND RESET FORM
+            $("#login-modal").hide();
+            $(".modal-backdrop").remove();
+            $("form").trigger("reset");
 
 
-        $(".close").click(function () {
-            $("#alert-login").hide();
 
+        }).catch(function (error) {
+            errorCode = error.code;
+            errorMsg = error.message;
+            if (errorCode === "auth/user-not-found") {
+
+                $("#alert-login-msg").html("Email not found. Have you registered yet?")
+                $("#alert-login").show();
+
+            }
+            if (errorCode === "auth/invalid-email") {
+
+                $("#alert-login-msg").html("Please input a valid email address.")
+                $("#alert-login").show();
+
+            }
+            if (errorCode === "auth/wrong-password") {
+
+                $("#alert-login-msg").html("Invalid password. Please check the password and try again.")
+                $("#alert-login").show();
+
+            }
+            if (errorCode === "auth/too-many-requests") {
+
+                $("#alert-login-msg").html("Too many requests. Please try again later.")
+                $("#alert-login").show();
+            }
+
+
+            $(".close").click(function () {
+                $("#alert-login").hide();
+
+            });
         });
-    });
 });
 
 //LOGOUT
