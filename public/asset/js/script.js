6cfa7df36c1f7be6c4bcae82ec6cc7aa5f590ec5
femke77 <megan.meyers.388@gmail.com>
2025-01-05 20:23:06 -0800

    updated deprecation warning

--- public/asset/js/script.js
+++ public/asset/js/script.js
@@ -1,13 +1,13 @@
 //Firebase Config
 var firebaseConfig = {
-    apiKey: "AIzaSyCh61fKpQfYBZ0MRgJcUfEcop57szkaAnc",
-    authDomain: "deploy-f0b72.firebaseapp.com",
-    databaseURL: "https://deploy-f0b72.firebaseio.com",
-    projectId: "deploy-f0b72",
-    storageBucket: "deploy-f0b72.appspot.com",
-    messagingSenderId: "280884255831",
-    appId: "1:280884255831:web:9b5914a751d3d6fb0f9712",
-    measurementId: "G-RFFRMPBZLJ"
+  apiKey: "AIzaSyCh61fKpQfYBZ0MRgJcUfEcop57szkaAnc",
+  authDomain: "deploy-f0b72.firebaseapp.com",
+  databaseURL: "https://deploy-f0b72.firebaseio.com",
+  projectId: "deploy-f0b72",
+  storageBucket: "deploy-f0b72.appspot.com",
+  messagingSenderId: "280884255831",
+  appId: "1:280884255831:web:9b5914a751d3d6fb0f9712",
+  measurementId: "G-RFFRMPBZLJ",
 };
 
 // Initialize Firebase
@@ -26,7 +26,6 @@ var page = 1;
 var limit = 10;
 var numPages;
 
-
 $("#prev-btn").hide();
 $("#next-btn").hide();
 
@@ -35,94 +34,105 @@ $("#next-btn").hide();
  * and find out if performer exists and has event coming up. Sorting is by date/time.
  */
 $("#ytSubmit").on("click", function (event) {
+  event.preventDefault();
+  $("#next-btn").prop("disabled", true);
+  $("#prev-btn").prop("disabled", true);
+  console.log("submit btn on click");
+  $("#event-section").empty();
+  input = $("#search-input").val().trim();
+  if (input === "") {
+    $("#modal-message").text(
+      "Please enter an artist or band name in the search bar."
+    );
+    $("#oops-modal").modal("show");
+  } else {
+    input = input.split(" ").join("-");
+    var queryURLPerformers =
+      "https://api.seatgeek.com/2/performers?slug=" +
+      input +
+      "&client_id=" +
+      seatGeekKey;
 
-    event.preventDefault();
-    $("#next-btn").prop("disabled", true);
-    $("#prev-btn").prop("disabled", true);
-    console.log("submit btn on click");
-    $("#event-section").empty();
-    input = $("#search-input").val().trim();
-    if (input === ""){
-        $("#modal-message").text("Please enter an artist or band name in the search bar.");
+    $.ajax({
+      url: queryURLPerformers,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+      if (response.performers.length === 0) {
+        $("#modal-message").text(
+          "Performer not found. Please check spelling and try again."
+        );
         $("#oops-modal").modal("show");
-    } else {
-        input = input.split(' ').join('-');
-        var queryURLPerformers = "https://api.seatgeek.com/2/performers?slug=" + input +
-            "&client_id=" + seatGeekKey;
+      } else {
+        var hasEvent = response.performers[0].has_upcoming_events;
+        imageURL = response.performers[0].image;
+        if (hasEvent === true) {
+          numEvents = response.performers[0].num_upcoming_events;
+          numPages = Math.ceil(numEvents / limit);
+          getEvents();
 
-        $.ajax({
-            url: queryURLPerformers,
-            method: "GET"
-        })
-            .then(function (response) {
-                console.log(response)
-                if (response.performers.length === 0) {
-                    $("#modal-message").text("Performer not found. Please check spelling and try again.");
-                    $("#oops-modal").modal("show");
-                } else {
-                    var hasEvent = response.performers[0].has_upcoming_events;
-                    imageURL = response.performers[0].image;
-                    if (hasEvent === true) {
-                        numEvents = response.performers[0].num_upcoming_events;
-                        numPages = Math.ceil(numEvents / limit);
-                        getEvents();
-
-                        if (numEvents > limit) {
-                            $("#next-btn").prop("disabled", false);
-                        }
-
-                    } else {
-                        $("#event-section").append("No upcoming events for this performer.")
-                    }
-                    ytSearch();
-                }
-            });
-    }
-   
+          if (numEvents > limit) {
+            $("#next-btn").prop("disabled", false);
+          }
+        } else {
+          $("#event-section").append("No upcoming events for this performer.");
+        }
+        ytSearch();
+      }
+    });
+  }
 });
 
 // HIDING THE RECOMMENDED BUTTON FROM THE USER.
 $("#rec").hide();
 
-// THIS FUNCTION WILL EXECUTE A SEARCH ON YOUTUBE ONCE AN ARTIST ENTERED IN TO THE SEARCH BAR AND THE SUBMIT BUTTON IS CLICKED. 
+// THIS FUNCTION WILL EXECUTE A SEARCH ON YOUTUBE ONCE AN ARTIST ENTERED IN TO THE SEARCH BAR AND THE SUBMIT BUTTON IS CLICKED.
 function ytSearch() {
-    var inputVal = $("#search-input").val().trim();
-    var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q=" + inputVal + "&key=" + apiKey;
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(queryURL);
-        console.log(response);
-        var results = response.items;
-        $("#video").empty();
-        $("#video2").empty();
-        for (var i = 0; i < results.length; i++) {
-            videoId = results[i].id.videoId;
-            console.log(videoId)
-            var videoFrame = $("<div class='videoFrame'>");
-            var video = $("<iframe allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen>");
-            video.attr("src", "https://www.youtube.com/embed/" + videoId);
-            videoFrame.append(video);
+  var inputVal = $("#search-input").val().trim();
+  var queryURL =
+    "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q=" +
+    inputVal +
+    "&key=" +
+    apiKey;
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(queryURL);
+    console.log(response);
+    var results = response.items;
+    $("#video").empty();
+    $("#video2").empty();
+    for (var i = 0; i < results.length; i++) {
+      videoId = results[i].id.videoId;
+      console.log(videoId);
+      var videoFrame = $("<div class='videoFrame'>");
+      var video = $(
+        "<iframe allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen>"
+      );
+      video.attr("src", "https://www.youtube.com/embed/" + videoId);
+      videoFrame.append(video);
 
-            var $eventList = $("<ul>");
-            $eventList.addClass("text-center");
-            $eventList.addClass("list-group");
-            $("#video").append($eventList);
-            var $eventListItem = $("<li class='list-group-item'>");
-            $eventListItem.append(videoFrame);
-            $eventListItem.append("<h4>" + "Title : " + results[i].snippet.title + "</h4>");
-            // LOOK INTO DOT DOT DOT LINK FOR DISCRIPTION!!!!
-            // $eventListItem.append("<h5>" + "Description : " + results[i].snippet.description + "</h5>");
-            $eventListItem.append("<h6>" + " Published : " + results[i].snippet.publishedAt + "</h6>");
+      var $eventList = $("<ul>");
+      $eventList.addClass("text-center");
+      $eventList.addClass("list-group");
+      $("#video").append($eventList);
+      var $eventListItem = $("<li class='list-group-item'>");
+      $eventListItem.append(videoFrame);
+      $eventListItem.append(
+        "<h4>" + "Title : " + results[i].snippet.title + "</h4>"
+      );
+      // LOOK INTO DOT DOT DOT LINK FOR DISCRIPTION!!!!
+      // $eventListItem.append("<h5>" + "Description : " + results[i].snippet.description + "</h5>");
+      $eventListItem.append(
+        "<h6>" + " Published : " + results[i].snippet.publishedAt + "</h6>"
+      );
 
+      $eventList.append($eventListItem);
 
-            $eventList.append($eventListItem);
-
-
-            $("#rec").show();
-        };
-    });
+      $("#rec").show();
+    }
+  });
 }
 
 /**
@@ -131,130 +141,132 @@ function ytSearch() {
  * list-group is used to display multiple events on the card in index
  */
 function getEvents() {
-    $("#event-section").empty();
-    var queryURLEvents = "https://api.seatgeek.com/2/events?performers.slug=" + input +
-        "&per_page=" + limit + "&page=" + page + "&client_id=" + seatGeekKey;
+  $("#event-section").empty();
+  var queryURLEvents =
+    "https://api.seatgeek.com/2/events?performers.slug=" +
+    input +
+    "&per_page=" +
+    limit +
+    "&page=" +
+    page +
+    "&client_id=" +
+    seatGeekKey;
 
-    $.ajax({
-        url: queryURLEvents,
-        method: "GET"
-    })
-        .then(function (response) {
-            console.log(response);
-            $("#prev-btn").show();
-            $("#next-btn").show();
-            var results = response.events;
-            for (let i = 0; i < results.length; i++) {
-                var $eventList = $("<ul>");
-                $eventList.addClass("list-group");
-                $("#event-section").append($eventList);
-                var $eventListItem = $("<li class='list-group-item'>");
-                $eventListItem.append("<h4>" + results[i].title + "</h4>");
-                if (results[i].date_tbd == false) {
-                    var dateTime = results[i].datetime_local;
-                    dateTime = dateTime.split('T').join(' ');
-                    var format = "YYYY/MM/DD hh:mm:ss";
-                    var convertedDateTime = moment(dateTime, format)
-                    $eventListItem.append(convertedDateTime.format("MM/DD/YY hh:mm A") + "<br/>");
-                } else {
-                    $eventListItem.append("Date and Time TBD")
-                }
-                $eventListItem.append("Venue: " + results[i].venue.name + "<br/>");
-                $eventListItem.append("Location: " + results[i].venue.display_location + "<br/>");
-                var $ticketBtn = $("<button>", {
-                    text: "Buy Tickets",
-                    class: "btn btn-primary",
-                    click: function () {
-                        window.open(results[i].url);
-                    }
-                })
-                $eventListItem.append($ticketBtn);
+  $.ajax({
+    url: queryURLEvents,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    $("#prev-btn").show();
+    $("#next-btn").show();
+    var results = response.events;
+    for (let i = 0; i < results.length; i++) {
+      var $eventList = $("<ul>");
+      $eventList.addClass("list-group");
+      $("#event-section").append($eventList);
+      var $eventListItem = $("<li class='list-group-item'>");
+      $eventListItem.append("<h4>" + results[i].title + "</h4>");
+      if (results[i].date_tbd == false) {
+        var dateTime = results[i].datetime_local;
+        dateTime = dateTime.split("T").join(" ");
+        var format = "YYYY/MM/DD hh:mm:ss";
+        var convertedDateTime = moment(dateTime, format);
+        $eventListItem.append(
+          convertedDateTime.format("MM/DD/YY hh:mm A") + "<br/>"
+        );
+      } else {
+        $eventListItem.append("Date and Time TBD");
+      }
+      $eventListItem.append("Venue: " + results[i].venue.name + "<br/>");
+      $eventListItem.append(
+        "Location: " + results[i].venue.display_location + "<br/>"
+      );
+      var $ticketBtn = $("<button>", {
+        text: "Buy Tickets",
+        class: "btn btn-primary",
+        click: function () {
+          window.open(results[i].url);
+        },
+      });
+      $eventListItem.append($ticketBtn);
 
-                $eventList.append($eventListItem);
-                
-            }
-            $(".main")[0].scrollIntoView();
-        });
-       
+      $eventList.append($eventListItem);
+    }
+    $(".main")[0].scrollIntoView();
+  });
 }
-
-
 
 //--PAGINATION METHODS FOR EVENT LISTS----------------------------
 $("#next-btn").on("click", function () {
-    if (page <= numPages) {
-        page++;
-        $("#prev-btn").prop("disabled", false)
-        getEvents();
-        if (page === numPages) {
-            $("#next-btn").prop("disabled", true);
-        }
+  if (page <= numPages) {
+    page++;
+    $("#prev-btn").prop("disabled", false);
+    getEvents();
+    if (page === numPages) {
+      $("#next-btn").prop("disabled", true);
     }
+  }
 });
 
 $("#prev-btn").on("click", function () {
-    if (page <= numPages) {
-        page--;
-        $("#next-btn").prop("disabled", false)
-        getEvents();
-        if (page === 1) {
-            $("#prev-btn").prop("disabled", true);
-        }
+  if (page <= numPages) {
+    page--;
+    $("#next-btn").prop("disabled", false);
+    getEvents();
+    if (page === 1) {
+      $("#prev-btn").prop("disabled", true);
     }
+  }
 });
 
 //------GET RECOMMENDED VIDEOS AND DISPLAY--------------------------------
 $("#rec").on("click", function (event) {
+  event.preventDefault();
 
+  var queryURL =
+    "https://www.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=" +
+    videoId +
+    "&type=video&key=" +
+    apiKey;
 
-    event.preventDefault();
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(queryURL);
+    console.log(response);
 
+    var results = response.items;
 
-    var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=" + videoId + "&type=video&key=" + apiKey;
+    var videoId;
+    $("#video2").empty();
+    for (var i = 0; i < results.length; i++) {
+      videoId = results[i].id.videoId;
+      console.log(videoId);
+      var videoFrame = $("<div class='videoFrame'>");
+      var video = $(
+        "<iframe allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen>"
+      );
+      video.attr("src", "https://www.youtube.com/embed/" + videoId);
+      videoFrame.append(video);
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-
-        console.log(queryURL);
-        console.log(response);
-
-        var results = response.items;
-
-        var videoId;
-        $("#video2").empty();
-        for (var i = 0; i < results.length; i++) {
-            videoId = results[i].id.videoId;
-            console.log(videoId)
-            var videoFrame = $("<div class='videoFrame'>");
-            var video = $("<iframe allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen>");
-            video.attr("src", "https://www.youtube.com/embed/" + videoId);
-            videoFrame.append(video);
-
-            var parTitle = results[i].snippet.title;
-            var $eventList = $("<ul>");
-            $eventList.addClass("float-left");
-            $eventList.css("width", "50%");
-            $eventList.addClass("text-center");
-            $eventList.addClass("list-group");
-            $("#video2").append($eventList);
-            var $eventListItem = $("<li class='list-group-item'>");
-            $eventListItem.append(videoFrame);
-            $eventListItem.append("<h4>" + "Title : " + results[i].snippet.title + "</h4>");
-            // $eventListItem.append("<h5>" + "Description : " + results[i].snippet.description + "</h5>");
-            $eventListItem.append("<h6>" + "Published : " + results[i].snippet.publishedAt + "</h6>");
-            $eventList.append($eventListItem);
-        }
-
-    });
-
+      var parTitle = results[i].snippet.title;
+      var $eventList = $("<ul>");
+      $eventList.addClass("float-left");
+      $eventList.css("width", "50%");
+      $eventList.addClass("text-center");
+      $eventList.addClass("list-group");
+      $("#video2").append($eventList);
+      var $eventListItem = $("<li class='list-group-item'>");
+      $eventListItem.append(videoFrame);
+      $eventListItem.append(
+        "<h4>" + "Title : " + results[i].snippet.title + "</h4>"
+      );
+      // $eventListItem.append("<h5>" + "Description : " + results[i].snippet.description + "</h5>");
+      $eventListItem.append(
+        "<h6>" + "Published : " + results[i].snippet.publishedAt + "</h6>"
+      );
+      $eventList.append($eventListItem);
+    }
+  });
 });
-
-
-
-
-
-
-
- 
